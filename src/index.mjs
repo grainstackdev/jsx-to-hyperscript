613aacf8f8dc165127626ae611de783e5352ecf3
loopt-mkv <99065516+loopt-mkv@users.noreply.github.com>
2023-01-25 18:38:36 -0800

    complete?

--- src/index.mjs
+++ src/index.mjs
@@ -3,40 +3,112 @@ import standardTags from "html-tags"
 
 const rand = Math.random().toString().slice(-2)
 
+const stringTypes = ["StringLiteral", "NoSubstitutionTemplate", 'RegularExpressionLiteral']
+function tokenizeStringLiteralClosed(token) {
+  if (stringTypes.includes(token.type) && token.closed) {
+    const beginning = token.value[0]
+    const end = token.value[token.value.length - 1]
+
+    const allowed = ['"', "'", "`", "/", "}", "{", "<", ">"]
+
+    if (allowed.includes(beginning) && allowed.includes(end)) {
+      const toTokens = Array.from(jsTokens(token.value.slice(1, -1)))
+      // console.log("toTokensClosed", toTokens)
+      return [
+        { type: "Punctuator", value: beginning },
+        ...toTokens,
+        { type: "Punctuator", value: end },
+      ].flatMap((t) => {
+        if (stringTypes.includes(t.type) && t.closed) {
+          return tokenizeStringLiteralClosed(t)
+        } else if (stringTypes.includes(t.type) && !t.closed) {
+          return tokenizeStringLiteralOpen(t)
+        }
+        return t
+      })
+    }
+  }
+  return [token]
+}
+
+function tokenizeStringLiteralOpen(token) {
+  if (stringTypes.includes(token.type) && !token.closed) {
+    const beginning = token.value[0]
+
+    const allowed = ['"', "'", "`", "/", "}", "{", "<", ">"]
+
+    if (allowed.includes(beginning)) {
+      const toTokens = Array.from(jsTokens(token.value.slice(1)))
+      // console.log("toTokensOpen", toTokens)
+      return [{ type: "Punctuator", value: beginning }, ...toTokens].flatMap(
+        (t) => {
+          if (stringTypes.includes(t.type) && t.closed) {
+            return tokenizeStringLiteralClosed(t)
+          } else if (stringTypes.includes(t.type) && !t.closed) {
+            return tokenizeStringLiteralOpen(t)
+          }
+          return t
+        }
+      )
+    }
+  }
+  return [token]
+}
+
 function splitTagEnds(tokens) {
   const out = []
+
   let i = -1
   for (const token of tokens) {
     i++
 
-    if (
-      token.type === "RegularExpressionLiteral" &&
-      /\/.*>/.test(token.value) &&
-      !token.closed
-    ) {
-      const prevToken = tokens[i - 1]
-      if (prevToken.type === "Punctuator" && prevToken.value === "<") {
-        // Possible matches:
-        // /div>
-        // />
-        // />);
-        const match = token.value.match(/\/.*>/)
-        const extra = token.value.slice(match[0].length)
+    // if (
+    //   token.type === "RegularExpressionLiteral" &&
+    //   /\/.*>/.test(token.value) &&
+    //   !token.closed
+    // ) {
+    //   const prevToken = tokens[i - 1]
+    //   if (prevToken.type === "Punctuator" && prevToken.value === "<") {
+    //     // Possible matches:
+    //     // /div>
+    //     // />
+    //     // />);
+    //     const match = token.value.match(/\/.*>/)
+    //     const extra = token.value.slice(match[0].length)
+    //
+    //     const extraTokens = Array.from(jsTokens(extra))
+    //
+    //     out.pop()
+    //     if (match[0] === "/>") {
+    //       // </>
+    //       out.push({ type: "HtmlEnd", value: `</Fragment_${rand}>` })
+    //     } else {
+    //       // </match0>
+    //       out.push({ type: "HtmlEnd", value: prevToken.value + match[0] })
+    //     }
+    //     extraTokens.forEach((a) => out.push(a))
+    //
+    //     continue
+    //   } else {
+    //     out.push({ type: "Punctuator", value: "/" })
+    //     const extra = token.value.slice(1)
+    //     const extraTokens = Array.from(jsTokens(extra))
+    //     extraTokens.forEach((a) => out.push(a))
+    //     continue
+    //   }
+    // }
 
-        const extraTokens = Array.from(jsTokens(extra))
-
-        out.pop()
-        if (match[0] === "/>") {
-          // </>
-          out.push({ type: "HtmlEnd", value: `</Fragment_${rand}>` })
-        } else {
-          // </match0>
-          out.push({ type: "HtmlEnd", value: prevToken.value + match[0] })
-        }
-        extraTokens.forEach((a) => out.push(a))
-
-        continue
+    const prevToken = tokens[i - 1]
+    const isPropValue = prevToken?.value === '=' && token.value[0] === '"'
+    if (stringTypes.includes(token.type) && !isPropValue) {
+      let extraTokens
+      if (token.closed) {
+        extraTokens = tokenizeStringLiteralClosed(token)
+      } else {
+        extraTokens = tokenizeStringLiteralOpen(token)
       }
+      extraTokens.forEach((a) => out.push(a))
+      continue
     }
 
     out.push(token)
@@ -45,6 +117,7 @@ function splitTagEnds(tokens) {
 }
 
 function findClosingHtmlBrace(tokens) {
+  // const validPunctuators = ["=", "{", "}", "<", "/", ">", " ", "\n", `"`, "'", "-", "_", "(", ")", ":"]
   const validPunctuators = ["=", "{", "}", "<", "/", ">", " ", "\n"]
 
   const stackBrace = []
@@ -54,9 +127,13 @@ function findClosingHtmlBrace(tokens) {
     if (token.type === "Punctuator" && token.value === "}") {
       stackBrace.pop()
     }
+    // if (token.type === "Punctuator" && token.value === '"') {
+    //   stackString.pop()
+    // }
 
     const atGlobal = stackBrace.length === 0
     if (atGlobal) {
+      // Things that are allowed to be seen at global level, i.e. not in "" or {}
       if (
         !(
           token.type === "IdentifierName" ||
@@ -85,6 +162,9 @@ function findClosingHtmlBrace(tokens) {
     if (token.type === "Punctuator" && token.value === "{") {
       stackBrace.push(true)
     }
+    // if (token.type === "Punctuator" && token.value === '"') {
+    //   stackString.pop()
+    // }
 
     if (
       token.type === "Punctuator" &&
@@ -258,12 +338,20 @@ function getTokens(str) {
     } else if (
       token.type === "Punctuator" &&
       token.value === "<" &&
-      nextToken.type === "RegularExpressionLiteral" &&
-      /^\/.*>/.test(nextToken.value)
+      nextToken.type === "Punctuator" &&
+      nextToken.value === '/'
     ) {
       // </div>
-      htmlTokens.push({ type: "HtmlEnd", value: token.value + nextToken.value })
-      _skip()
+      const closingIndex = findClosingHtmlBrace(tokens.slice(i))
+      if (closingIndex !== -1) {
+        const endTagTokens = tokens.slice(i, i + closingIndex + 1)
+        htmlTokens.push({ type: "HtmlEnd", value: endTagTokens.map(t => t.value).join('') })
+        endTagTokens.forEach((_, i) => {
+          if (i <= endTagTokens.length - 2) {
+            _skip()
+          }
+        })
+      }
     } else {
       htmlTokens.push(tokens[i])
     }
@@ -271,7 +359,13 @@ function getTokens(str) {
     i += 1 + skip
   }
 
+  // for (const token of htmlTokens) {
+  //   console.log('token', token)
+  // }
   const result = extendContext(htmlTokens)
+  // for (const token of result) {
+  //   console.log('token2', token)
+  // }
   return result
 }
 
@@ -283,17 +377,56 @@ function extendContext(tokens) {
 
   const result = []
 
+  let startClip = null
+  function endClip(endI) {
+    if (startClip === null) return
+    const clip = tokens.slice(startClip, endI)
+    startClip = null
+
+    // now that I have the clips of string literals between closing braces > and curly braces {,
+    // what do I do with them?
+    // I want to join them all together as a single token, and then label it with isHtmlStringLiteral
+    // result stopped pushing tokens when the clip started.
+
+    result.push({
+      type: "HtmlStringLiteral",
+      value: clip.map((t) => t.value).join(""),
+      context: {
+        stackBackTick: stackBackTick.length,
+        stackParen: stackParen.length,
+        stackBrace: stackBrace.length,
+        stackHtml: stackHtml.length,
+      },
+    })
+    result.push({
+      ...tokens[endI],
+      context: {
+        stackBackTick: stackBackTick.length,
+        stackParen: stackParen.length,
+        stackBrace: stackBrace.length,
+        stackHtml: stackHtml.length,
+      },
+    })
+  }
+
+  let lastHtmlStart = null
+
   let i = -1
   for (const token of tokens) {
     i++
 
-    if (token.type === "TemplateTail") {
+    let startClipFlag = false
+
+    if (startClip === null && token.type === "TemplateTail") {
       stackBackTick.pop()
     }
-    if (token.value === ")") {
+    if (startClip === null && token.value === ")") {
       stackParen.pop()
     }
-    if (token.value === "}") {
+    if (startClip === null && token.value === "}") {
+      if (!token.insideOpeningTag) {
+        startClipFlag = true
+      }
       stackBrace.pop()
     }
     let htmlStart
@@ -301,8 +434,21 @@ function extendContext(tokens) {
       const isSelfClosing = token.htmlStart?.selfClosing
       if (!isSelfClosing) {
         const popToken = stackHtml.pop()
-        htmlStart = popToken.htmlStart
+        if (!popToken) {
+          htmlStart = lastHtmlStart
+          lastHtmlStart = null
+        } else {
+          htmlStart = popToken.htmlStart
+        }
       }
+
+      if (stackHtml.length > 0) {
+        startClipFlag = true
+      }
+      endClip(i)
+      // pop because endClip adds this token.
+      // If endClip is called before the result.push, then a pop must proceed it.
+      result.pop()
     }
 
     const forward = {
@@ -317,22 +463,44 @@ function extendContext(tokens) {
     if (htmlStart) {
       forward.htmlStart = htmlStart
     }
-    result.push(forward)
+    // console.log('startClip', token.type, token.value, startClip, stackHtml.length)
+    if (startClip === null) {
+      result.push(forward)
+    }
 
-    if (token.type === "TemplateHead") {
+    if (startClip === null && token.type === "TemplateHead") {
       stackBackTick.push(true)
     }
-    if (token.value === "(") {
+    if (startClip === null && token.value === "(") {
       stackParen.push(true)
     }
     if (token.value === "{") {
+      endClip(i)
       stackBrace.push(true)
     }
-    if (token.type === "HtmlStart" && token.value === `<Fragment_${rand}>`) {
+    if (token.type === "HtmlStart") {
+      lastHtmlStart = token
+      endClip(i)
+    }
+    if (
+      startClip === null &&
+      token.type === "HtmlStart" &&
+      token.value === `<Fragment_${rand}>`
+    ) {
+      lastHtmlStart = token
       stackHtml.push(token)
     }
-    if (token.type === "HtmlStartClosingBrace" && token.value === ">") {
+    if (
+      startClip === null &&
+      token.type === "HtmlStartClosingBrace" &&
+      token.value === ">"
+    ) {
+      startClipFlag = true
       stackHtml.push(token)
+    }
+
+    if (startClipFlag) {
+      startClip = i + 1
     }
   }
 
@@ -341,7 +509,9 @@ function extendContext(tokens) {
 
 function transform(str, factory) {
   let tokens = getTokens(str)
-  tokens = extendContext(tokens)
+  // tokens = extendContext(tokens)
+
+  // console.log('tokens', tokens)
 
   const transformedTokens = []
   let startClip = null
@@ -361,8 +531,7 @@ function transform(str, factory) {
       const endClip = i + 1
 
       const clip = tokens.slice(startClip, endClip)
-      const html = clip.map((t) => t.value).join("")
-      const base = _transform(html, factory)
+      const base = _transform(clip, factory)
       const baseTokens = Array.from(jsTokens(base))
       baseTokens.forEach((t) => transformedTokens.push(t))
       startClip = null
@@ -434,12 +603,24 @@ function getProps(tokens, factory) {
 }
 
 // input is nested HTML in the form of tokens
-function _transform(str, factory) {
-  let tokens = getTokens(str)
-  tokens = extendContext(tokens)
+function _transform(tokens, factory) {
+  // let tokens = getTokens(str)
+  // tokens = extendContext(tokens)
 
   let lines = ""
   let stackOffsetBrace = 0
+
+  function addComma() {
+    if (
+      lines.slice(-1) === ")" ||
+      lines.slice(-1) === "]" ||
+      lines.slice(-1) === "'" ||
+      lines.slice(-1) === `"` ||
+      (lines.slice(-2, -1) === `*` && lines.slice(-1) === `/`)
+    ) {
+      lines += ", "
+    }
+  }
 
   let i = -1
   for (const token of tokens) {
@@ -482,7 +663,7 @@ function _transform(str, factory) {
           if (isFragment) {
             line = `[`
           } else {
-            if (propsAsString !== 'null') {
+            if (propsAsString !== "null") {
               line = `${tagName}(${propsAsString})`
             } else {
               line = `${tagName}()`
@@ -490,26 +671,30 @@ function _transform(str, factory) {
           }
         }
 
-        if (lines.slice(-1) === ")" || lines.slice(-1) === "]") {
-          lines += ", "
-        }
+        addComma()
         lines += line
+      }
+      if (token.type === "HtmlStringLiteral") {
+        const value = token.value.replace(/\s/g, '')
+        if (value) {
+          addComma()
+          lines += JSON.stringify(value)
+        }
       }
       if (
         token.type === "Punctuator" &&
         token.value === "{" &&
         !token.insideOpeningTag
       ) {
+        // There is an expression between curly braces which needs to be
+        // added as an element in the array of children.
         const index = findCurlyBraceEnd(tokens.slice(i))
-        const code = tokens
+        const curlyCode = tokens
           .slice(i + 1, i + index)
           .map((t) => t.value)
           .join("")
-
-        if (lines.slice(-1) === ")" || lines.slice(-1) === "]") {
-          lines += ", "
-        }
-        lines += code
+        addComma()
+        lines += curlyCode
       }
       if (token.type === "HtmlEnd") {
         const tagName = tokens[i]?.htmlStart?.value?.replace(/[<>\/]/g, "")
