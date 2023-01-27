bed6b2a0c22c80ee6ca8f4b190da3ad0ce1492c1
loopt-mkv <99065516+loopt-mkv@users.noreply.github.com>
2023-01-26 22:39:43 -0800

    add test harness

---
+++ tests/component.test.js
@@ -0,0 +1,22 @@
+// Instances of React are transform as well
+import React from "hyperscript"
+
+const signs = [{ value: "+" }, { value: "-" }]
+
+const renderSign = (props) => {
+  return (
+    <span>{props.value}</span>
+  )
+}
+
+const Component = () => (<div></div>)
+const test = (
+  <div style={{color: 'orange'}}>
+    <Component />
+    <button onClick={() => {
+      const el = <div/>
+      console?.log(el)
+    }} />
+    {signs.map(renderSign)}
+  </div>
+)
---
+++ tests/test.js
@@ -0,0 +1,53 @@
+import test from 'tape'
+import jsxConvert from "../src/index.mjs";
+import fs from 'fs'
+import path from 'path'
+
+const fragmentsAnswer = `function renderRow() {
+  return (
+    [h('span', null, []), h('span', null, [])]
+  )
+}`
+
+const componentsAnswer = `// Instances of h are transform as well
+import h from "hyperscript"
+
+const signs = [{ value: "+" }, { value: "-" }]
+
+const renderSign = (props) => {
+  return (
+    h('span', null, [props.value])
+  )
+}
+
+const Component = () => (h('div', null, []))
+const test = (
+  h('div', {style: {color: 'orange'}}, [Component(), h('button', {onClick: () => {
+      const el = h('div', null, [])
+      console?.log(el)
+    }}, []), signs.map(renderSign)])
+)`
+
+test('components', (t) => {
+  const str = fs.readFileSync(path.resolve(process.cwd(), 'tests/component.test.js'), {encoding: 'utf8'})
+
+  const out = jsxConvert(str, {
+    factory: 'h'
+  })
+
+  t.equal(out, componentsAnswer)
+
+  t.end()
+})
+
+test('fragments', (t) => {
+  const str = fs.readFileSync(path.resolve(process.cwd(), 'tests/fragment.test.js'), {encoding: 'utf8'})
+
+  const out = jsxConvert(str, {
+    factory: 'h'
+  })
+
+  t.equal(out, fragmentsAnswer)
+
+  t.end()
+})
